e9fe92753c8e361fbdde22d8a987a6f1760b53ee
JaumC <jvvianachaves@gmail.com>
2025-05-02 15:27:04 -0300

    recuperar desafio singular

--- src/services/dare/get_dare.ts
+++ src/services/dare/get_dare.ts
@@ -2,38 +2,45 @@ import { Request, Response } from "express";
 import Dare from "../../models/Dare";
 import User from "../../models/User";
 import DayPoint from "../../models/DayPoint";
+import { Types } from "mongoose";
 
 export const get_dare = async (req: Request, res: Response) => {
     const { id } = req.params;
+    const { dareId } = req.query;
 
     if (!id) {
         res.status(400).json({ message: 'ID faltando.' });
         return;
     }
 
-    const user = await User.findById({ _id: id })
+    const user = await User.findById({ _id: id });
 
     if (!user) {
-        res.status(404).json({ message: 'Usuário não encontrado.' })
-        return;
-    }
-    const dare = await Dare.find({
-        $or: [
-            { host: user._id },
-            { challengers: user._id },
-        ]
-    })
-
-    if (!dare) {
-        res.status(404).json({ message: 'Desafio não encontrado.' })
+        res.status(404).json({ message: 'Usuário não encontrado.' });
         return;
     }
 
-    const dayPoint = await DayPoint.find({ dare_id: { $in: dare.map(d => d._id) } })
-    if (!dayPoint) {
-        res.status(404).json({ message: 'Pontos diários não encontrados.' })
-        return;
+    let dareList: any[] = [];
+
+    if (!dareId) {
+        dareList = await Dare.find({
+            $or: [
+                { host: user._id },
+                { challengers: user._id },
+            ]
+        });
+    } else {
+        const dare = await Dare.findOne({ _id: dareId });
+        if (!dare) {
+            res.status(404).json({ message: 'Desafio não encontrado.' });
+            return;
+        }
+        dareList = [dare];
     }
 
-    res.status(200).json({ dare, dayPoint });
-}
+    const dareIds = dareList.map((d) => d._id);
+
+    const dayPoint = await DayPoint.find({ dare_id: { $in: dareIds } });
+
+    res.status(200).json({ dare: dareList, dayPoint });
+};
